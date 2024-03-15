3a6596267a59525464f7b464ce477f7b66691e9c
EduardoCM <ecm3198@gmail.com>
2024-03-15 08:31:42 -0600

    Update App.js
    update titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         - Welcome Java Full Stack Developer - UPA -
+         - Welcome Java Full Stack Developer - UPA - Codigo Rupestre Space Native
         </p>
         <a
           className="App-link"
